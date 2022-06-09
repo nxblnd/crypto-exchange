e8c02c57b90b8559a420df649bb26cd58b00edc0
nxblnd <nxblnd@arch.localdomain>
2022-06-09 18:04:54 +0300

    Somewhat working custom select

--- src/components/CurrencySelector.vue
+++ src/components/CurrencySelector.vue
@@ -1,16 +1,50 @@
 <template>
-  <select v-model="selected">
-      <option v-for="currency in currencies" :key="currency.ticker">{{ currency.ticker }} | {{ currency.name }}</option>
-  </select>
+  <div class="currency-selector relative">
+    <div class="selected flex">
+      <input class="amount bg-inherit" v-model.number="amount" placeholder="0"/>
+      <div class="currency" @click="toggleList">{{ selected.ticker }}</div>
+    </div>
+    <div class="available-items absolute" :class="{ hidden: !isOpen }">
+      <div 
+        v-for="(currency, i) in currencies"
+        :key="i"
+        @click="setSelected(currency)"
+        class="hover:bg-sky-200"
+      >
+        {{ currency.ticker }} | {{ currency.name }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 
 export default {
-  props: ['currencies']
+  props: ['currencies'],
+  
+  data() {
+    return {
+      selected: { ticker: 'sel' },
+      amount: null,
+      isOpen: false,
+    }
+  },
+
+  methods: {
+    setSelected(currency) {
+      this.selected = currency;
+      this.toggleList();
+    },
+    toggleList() {
+      this.isOpen = !this.isOpen;
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.hidden {
+  display: none;
+}
 
 </style>

e9d9ac1a93639160f4cb58f0d559dba598bc15be
nxblnd <nxblnd@arch.localdomain>
2022-06-10 12:39:13 +0300

    Added flexbox container to currency selector

--- src/components/CurrencySelector.vue
+++ src/components/CurrencySelector.vue
@@ -2,7 +2,18 @@
   <div class="relative max-w-md bg-slate-100 outline outline-1 outline-solid outline-slate-400 hover:outline-slate-500 select-none" :class="{ rounded: !isOpen, 'rounded-t': isOpen }" tabindex="0" @blur="toggleList">
     <div class="flex flex-wrap p-2.5">
       <input class="grow bg-inherit focus:outline-none focus-visible:outline-none" v-model.number="amount" placeholder="0"/>
-      <div class="w-40 min-w-fit" @click="toggleList">{{ selected.ticker }}</div>
+      <div class="flex w-40 min-w-fit" @click="toggleList">
+        <template v-if="selected">
+          <p>{{ selected.ticker }}</p>
+        </template>
+        <template v-else>
+          <p class="text-slate-500">BTC</p>
+        </template>
+        <div class="ml-auto self-center text-slate-500">
+          <ChevronDownIcon v-if="!isOpen" class="h-5 w-5" />
+          <XIcon v-if="isOpen" class="h-4 w-4" />
+        </div>
+      </div>
     </div>
     <div class="absolute bg-inherit left-0 right-0 overflow-hidden outline outline-1 rounded-b outline-solid outline-slate-400 hover:outline-slate-500 z-10" :class="{ hidden: !isOpen }">
       <div 
@@ -18,13 +29,16 @@
 </template>
 
 <script>
+import { ChevronDownIcon, XIcon } from '@heroicons/vue/solid'
 
 export default {
+  components: { ChevronDownIcon, XIcon },
+
   props: ['currencies'],
   
   data() {
     return {
-      selected: { ticker: 'sel' },
+      selected: null,
       amount: null,
       isOpen: false,
     }

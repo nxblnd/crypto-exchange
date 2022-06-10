1d0a5b8723814b61d1040b21d7d1ee58a74d6b53
nxblnd <nxblnd@arch.localdomain>
2022-06-10 14:16:39 +0300

    Small list toggling bugfix

--- src/components/CurrencySelector.vue
+++ src/components/CurrencySelector.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative max-w-md bg-slate-100 outline outline-1 outline-solid outline-slate-400 hover:outline-slate-500 select-none" :class="{ rounded: !isOpen, 'rounded-t': isOpen }" tabindex="0" @blur="toggleList">
+  <div class="relative max-w-md bg-slate-100 outline outline-1 outline-solid outline-slate-400 hover:outline-slate-600 select-none" :class="{ rounded: !isOpen, 'rounded-t': isOpen }" tabindex="0" @blur="isOpen = false">
     <div class="flex flex-wrap p-2.5">
       <input class="grow bg-inherit focus:outline-none focus-visible:outline-none placeholder:text-slate-400" v-model.number="amount" placeholder="0"/>
       <div class="flex justify-center gap-5 w-40 min-w-fit before:block before:border-l before:border-solid before:border-slate-400" @click="toggleList">

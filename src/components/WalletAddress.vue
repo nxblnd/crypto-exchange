5ed920a61b398765e8479bc3ab5c285535cb1e5b
nxblnd <nxblnd@arch.localdomain>
2022-06-10 18:46:17 +0300

    Filled wallet address component with tags

--- src/components/WalletAddress.vue
+++ src/components/WalletAddress.vue
@@ -1,5 +1,9 @@
 <template>
   <p>Wallet Address</p>
+  <div class="flex gap-5 flex-wrap md:flex-nowrap">
+    <input class="input grow" />
+    <button class="btn grow md:grow-0">Exchange</button>
+  </div>
 </template>
 
 <script>

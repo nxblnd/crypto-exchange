afbfba0e45fca6d7304f70ec3f6d9701e0a38a9f
nxblnd <nxblnd@arch.localdomain>
2022-06-10 16:20:36 +0300

    Reordered tags in main SFC

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup>
-  import ConverterWidget from './components/ConverterWidget.vue';
-</script>
-
 <template>
   <header class="container mx-auto p-4">
     <h1 class="text-5xl font-light leading-relaxed">Crypto Exchange</h1>
@@ -13,8 +9,13 @@
   </main>
 </template>
 
+<script setup>
+  import ConverterWidget from './components/ConverterWidget.vue';
+</script>
+
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Vollkorn&display=swap');
+  
   body {
     font-family: 'Vollkorn', serif;
   }

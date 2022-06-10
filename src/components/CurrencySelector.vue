9a9970553dd94610b470d2d42bc7b935f9b48401
nxblnd <nxblnd@arch.localdomain>
2022-06-10 13:58:48 +0300

    Improved alignments in selector

--- src/components/CurrencySelector.vue
+++ src/components/CurrencySelector.vue
@@ -2,17 +2,17 @@
   <div class="relative max-w-md bg-slate-100 outline outline-1 outline-solid outline-slate-400 hover:outline-slate-500 select-none" :class="{ rounded: !isOpen, 'rounded-t': isOpen }" tabindex="0" @blur="toggleList">
     <div class="flex flex-wrap p-2.5">
       <input class="grow bg-inherit focus:outline-none focus-visible:outline-none" v-model.number="amount" placeholder="0"/>
-      <div class="flex w-40 min-w-fit" @click="toggleList">
+      <div class="flex justify-center gap-5 w-40 min-w-fit before:block before:border-l before:border-solid before:border-slate-400" @click="toggleList">
         <template v-if="selected">
-          <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg"><image class="p-1" :href="selected.image" /></svg>
+          <svg class="self-center ml-auto h-5 w-5" xmlns="http://www.w3.org/2000/svg"><image class="m-1" :href="selected.image" /></svg>
           <p>{{ selected.ticker }}</p>
         </template>
         <template v-else>
-          <p class="text-slate-500">BTC</p>
+          <p class="ml-auto text-slate-500">BTC</p>
         </template>
-        <div class="ml-auto self-center text-slate-500">
-          <ChevronDownIcon v-if="!isOpen" class="h-5 w-5" />
-          <XIcon v-if="isOpen" class="h-4 w-4" />
+        <div class="ml-auto self-center h-5 w-5 text-slate-500">
+          <ChevronDownIcon v-if="!isOpen" />
+          <XIcon v-if="isOpen" />
         </div>
       </div>
     </div>
